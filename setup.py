018e176f9d32103c54552a0d2fc5d6681f4b4382
Calin Crisan <ccrisan@gmail.com>
2019-12-19 23:22:37 +0200

    setup.py: fix version

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 
 from setuptools import setup, find_namespace_packages
 
+from qtoggleserver.dallastemp import VERSION
+
 
 setup(
     name='qtoggleserver-rpigpio',
-    version='1.0.0',
+    version=VERSION,
     description='qToggleServer driver for Raspberry Pi GPIOs',
     author='Calin Crisan',
     author_email='ccrisan@gmail.com',
